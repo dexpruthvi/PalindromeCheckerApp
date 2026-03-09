d36fe365f8d711985c8fb32c6800afaa182aea56
dexpruthvi <pruthvisamane@gmail.com>
2026-03-09 10:26:49 +0530

    UC4: Case insensitive palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -11,13 +11,15 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
+        // convert to lowercase for case-insensitive check
+        String original = word.toLowerCase();
         String reversed = "";
 
-        for(int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for(int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
         }
 
-        if(word.equals(reversed)) {
+        if(original.equals(reversed)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is NOT a Palindrome");
