40728fcf9d6c74878bb4a6d492989201775e773d
dexpruthvi <pruthvisamane@gmail.com>
2026-03-09 10:25:59 +0530

    UC3: Palindrome check using user input

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,21 +1,28 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker Application");
 
-        String word = "madam";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
+        for(int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
 
-        if (word.equals(reversed)) {
+        if(word.equals(reversed)) {
             System.out.println(word + " is a Palindrome");
         } else {
-            System.out.println(word + " is not a Palindrome");
+            System.out.println(word + " is NOT a Palindrome");
         }
 
+        scanner.close();
     }
 }
